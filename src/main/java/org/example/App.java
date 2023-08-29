6be38e99ae8415cb4ca6de9ccd532800c5cae0fe
ChangeXin <99721508+ChangeXin@users.noreply.github.com>
2023-08-29 16:09:05 +0800

    Update App.java
    16.08

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 public class App {
     public static void main(String[] args)  {
         System.out.println("hellowrold");
-        System.out.println("hellowrold");
+        System.out.println("hellowrold");  System.out.println("hellowrold");
      
     }
 }
